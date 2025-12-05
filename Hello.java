da9cafd3bfaa2bd47a6bf0d6c871ee979ffe0cee
suhasdk18 <suhasdk18@gmail.com>
2025-12-05 10:15:03 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+
+    // The main method is the entry point for all Java applications
+    public static void main(String[] args) {
+        // Print a message to the console
+        System.out.println("Hello, Java from Jenkins!");
+    }
+}
